d34ffb9f0e922ede080c604ab6d692fa85229c6d
Anatolii Voloshyn <anatolii.voloshyn@marmend.com>
2025-04-15 16:48:04 +0300

    fixed deps nitro

--- lib/application.ex
+++ lib/application.ex
@@ -2,13 +2,22 @@ defmodule ElixirSample.Application do
   require N2O
   use Application
   def start(_type, _args) do
+    dispatch = :cowboy_router.compile([
+      {:_, [
+        {"/favicon.ico", :cowboy_static, {:file, "priv/static/favicon.ico", []}},
+        {"/n2o/[...]",   :cowboy_static, {:dir, "deps/n2o/priv", []}},
+        {"/app/[...]",   :cowboy_static, {:dir, "priv/static", []}},
+        {"/ws/[...]",    :n2o_cowboy, []},
+        {"/nitro/[...]", :cowboy_static, {:dir, "deps/nitro/priv", []}}
+      ]}
+    ])
+
     :cowboy.start_clear(
       :http,
       [{:port, 8001}],
-      %{env: %{dispatch: :n2o_cowboy.points()}}
+      %{env: %{dispatch: dispatch}}
     )
 
-    IO.inspect(:n2o_cowboy.points())
     Supervisor.start_link([], strategy: :one_for_one)
   end
 end
